4dc93a241ed0748f7e25ab7a3a019ecdfd6c2aad
Brownie <cristenjohnson@rocketmail.com>
2023-01-26 15:46:03 -0600

    practicing

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,18 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void testEmptyString(){
+        //Test case ---> ""
+        //
+        assertTrue(BalancedBrackets.hasBalancedBrackets( ""));
+    }
 
+    @Test
+    public void singlePairOfBalancedBrackets(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets( "[]"));
+    }
+
+    @Test
+    public void testThreePairsOf
 }
